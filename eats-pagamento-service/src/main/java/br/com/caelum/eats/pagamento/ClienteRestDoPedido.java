026e5d6a6f14741e0c2306086eae3225e919663a
unknown <tatisimoes86@gmail.com>
2022-03-10 22:09:34 -0300

    Implementação OpenFeign

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/ClienteRestDoPedido.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/ClienteRestDoPedido.java
@@ -2,38 +2,38 @@ package br.com.caelum.eats.pagamento;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.cloud.openfeign.FeignClient;
 
-import java.util.Map;
 
 @Service
 public class ClienteRestDoPedido {
 
-    private final String monolitoUrl;
-    private final RestTemplate restTemplate;
-
-    public ClienteRestDoPedido(@Value("${configuracao.monolito.url}") String monolitoUrl, RestTemplate restTemplate) {
-        this.monolitoUrl = monolitoUrl;
-        this.restTemplate = restTemplate;
-    }
+    @Autowired
+    private PedidoClient pedidoClient;
 
     void notificaPagamentoDoPedido(Long pedidoId) {
-        String endpoint = this.monolitoUrl.concat(String.format("/pedidos/%d/status", pedidoId));
 
-        ResponseEntity<?> response = restTemplate.exchange(endpoint, HttpMethod.PUT, new HttpEntity<>(new PedidoMudancaDeStatusRequest("pago".toUpperCase())), Map.class);
-
-//        this.restTemplate.put(endpoint, new PedidoMudancaDeStatusRequest("pago".toUpperCase()));
+        ResponseEntity<?> response = pedidoClient.atualizaStatusPedido(pedidoId, new PedidoMudancaDeStatusRequest("pago".toUpperCase()));
 
         if (!HttpStatus.valueOf(response.getStatusCodeValue()).is2xxSuccessful()) {
             throw new RuntimeException("problema ao tentar mudar o status do pedido: " + pedidoId);
         }
     }
+}
+
+@FeignClient("monolito")
+interface PedidoClient {
+
+    @PutMapping("/pedidos/{numeroDoPedido}/status")
+    ResponseEntity<?> atualizaStatusPedido(@PathVariable Long pedidoId, @RequestBody PedidoMudancaDeStatusRequest pedidoMudandaStatus);
 }
 
 @Getter
